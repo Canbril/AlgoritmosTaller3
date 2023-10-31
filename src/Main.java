0dc0cd06dae09ef418d1582eee964b8347673a34
Carlos Esteban Sánchez <141968266+carlosSanchez07@users.noreply.github.com>
2023-10-31 16:05:55 -0500

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,156 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            int rows = 4096;
+            int columns = 4096;
+            int[][] A = new int[rows][columns];
+            int[][] B = new int[rows][columns];
+            int[][] Result = new int[rows][columns];
+            llenarMatriz(A,rows, columns);
+            llenarMatriz(B, rows, columns);
+
+        long startTime = System.nanoTime();
+        //NaivStandard(A,B,Result,rows,rows, columns);
+        //NaivLoopUnrollingThree(A,B,Result,rows,rows,columns);
+        NaivLoopUnrollingFour(A,B,Result,rows, rows, columns);
+        long endTime = System.nanoTime();
+        long elapsedTime = endTime - startTime;
+        System.out.println("Tiempo de ejecución: " + (elapsedTime / 1000000) + " milisegundos");
+        //imprimirMatriz(Result,rows, columns);
+
+
+
+
     }
+
+    public static class StrassenWinograd {
+
+
+    }
+
+    public static void NaivLoopUnrollingFour(int[][] A, int[][] B, int[][] Result, int N, int P, int M) {
+        int i, j, k;
+        int aux;
+        if (P % 4 == 0) {
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < M; j++) {
+                    aux = 0;
+                    for (k = 0; k < P; k += 4) {
+                        aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j] + A[i][k + 3] * B[k + 3][j];
+                    }
+                    Result[i][j] = aux;
+                }
+            }
+        } else if (P % 4 == 1) {
+            int PP = P - 1;
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < M; j++) {
+                    aux = 0;
+                    for (k = 0; k < PP; k += 4) {
+                        aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j] + A[i][k + 3] * B[k + 3][j];
+                    }
+                    Result[i][j] = aux + A[i][PP] * B[PP][j];
+                }
+            }
+        } else if (P % 4 == 2) {
+            int PP = P - 2;
+            int PPP = P - 1;
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < M; j++) {
+                    aux = 0;
+                    for (k = 0; k < PP; k += 4) {
+                        aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j] + A[i][k + 3] * B[k + 3][j];
+                    }
+                    Result[i][j] = aux + A[i][PP] * B[PP][j] + A[i][PPP] * B[PPP][j];
+                }
+            }
+        } else {
+            int PP = P - 3;
+            int PPP = P - 2;
+            int PPPP = P - 1;
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < M; j++) {
+                    aux = 0;
+                    for (k = 0; k < PP; k += 4) {
+                        aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j] + A[i][k + 3] * B[k + 3][j];
+                    }
+                    Result[i][j] = aux + A[i][PP] * B[PP][j] + A[i][PPP] * B[PPP][j] + A[i][PPPP] * B[PPPP][j];
+                }
+            }
+        }
+    }
+
+    public static void NaivLoopUnrollingThree(int[][] A, int[][] B, int[][] Result, int N, int P, int M) {
+        int i, j, k;
+        int aux;
+        if (P % 3 == 0) {
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < M; j++) {
+                    aux = 0;
+                    for (k = 0; k < P; k += 3) {
+                        aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j];
+                    }
+                    Result[i][j] = aux;
+                }
+            }
+        } else if (P % 3 == 1) {
+            int PP = P - 1;
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < M; j++) {
+                    aux = 0;
+                    for (k = 0; k < PP; k += 3) {
+                        aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j];
+                    }
+                    Result[i][j] = aux + A[i][PP] * B[PP][j];
+                }
+            }
+        } else {
+            int PP = P - 2;
+            int PPP = P - 1;
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < M; j++) {
+                    aux = 0;
+                    for (k = 0; k < PP; k += 3) {
+                        aux += A[i][k] * B[k][j] + A[i][k + 1] * B[k + 1][j] + A[i][k + 2] * B[k + 2][j];
+                    }
+                    Result[i][j] = aux + A[i][PP] * B[PP][j] + A[i][PPP] * B[PPP][j];
+                }
+            }
+        }
+    }
+
+        public static void llenarMatriz(int[][] matrix, int rows, int columns){
+            //Llenar la matriz
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    matrix[i][j] = (int) (Math.random() * 999);
+                }
+            }
+        }
+
+    public static void imprimirMatriz(int[][] matrix, int rows, int columns){
+        //Llenar la matriz
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void NaivStandard(int[][] A, int[][] B, int[][] Result, int N, int P, int M) {
+        int aux;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                aux = 0;
+                for (int k = 0; k < P; k++) {
+                    aux += A[i][k] * B[k][j];
+                }
+                Result[i][j] = aux;
+            }
+        }
+
+    }
+
 }
